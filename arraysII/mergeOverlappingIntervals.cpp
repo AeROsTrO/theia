7d3b126eb312c937ebe9f27ac8569217e6d07f28
Kongara Anshu <anshu.kongara@gmail.com>
2023-02-17 19:41:38 +0530

    ek aur approach

--- arraysII/mergeOverlappingIntervals.cpp
+++ arraysII/mergeOverlappingIntervals.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 //problem description here
 // Given an array of intervals, merge all the overlapping intervals and return an array of non-overlapping intervals.
-
+void etTuBrute(vector<vector<int>>&v){
+    int n=v.size();
+    sort(v.begin(), v.end());
+    vector<vector<int>>ans;
+    for(int i=0; i<n; i++){
+        int st=v[i][0], end=v[i][1];
+        if(!ans.empty()){
+            if(st<=ans.back().back())continue;
+        }
+        for(int j=i+1; j<n; j++){
+            if(v[j][0]<=end){
+                end=max(end, v[j][1]);
+            }
+        }
+        end=max(end, v[i][1]);
+        ans.push_back({st, end});
+    }
+   for(int i=0; i<ans.size(); ++i)cout<<ans[i][0]<<" "<<ans[i][1]<<endl;
+}
 void optimal(vector<vector<int>>&v){
     vector<vector<int>>ans;
     int n=v.size();
@@ -24,6 +42,6 @@ int n;
 cin>>n;
 vector<vector<int>>v(n, vector<int>(2));
 for(int i=0; i<n; i++)cin>>v[i][0]>>v[i][1];
-optimal(v); 
+etTuBrute(v); 
 return 0;
 }
